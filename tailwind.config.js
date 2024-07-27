e8edeb92d98011361e24c4c0906d786cb94f34d8
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-07-27 14:25:30 +0200

    add design system in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      red: "#FC4747",
+      black: "#10141E",
+      lightGrey: "#5A698F",
+      darkGrey: "#161D2F",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+    },
+    extend: {
+      fontSize: {
+        "3xl": "2rem",
+        base: "0.9375rem",
+      },
+    },
   },
   plugins: [],
-};
+}
