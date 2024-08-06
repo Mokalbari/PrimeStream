01dab6213349bb3bed40f02cd46c709724ecf9cd
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-08-06 20:21:54 +0200

    add conditional display for movies in query

--- src/pages/Query.tsx
+++ src/pages/Query.tsx
@@ -1,13 +1,22 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, useParams } from "react-router-dom"
+import formatName from "../components/functions/formatName"
 import DisplaySelectionSection from "../components/DisplaySelectionSection"
 import type { APIDataType } from "../components/types/APIDataType"
 
 const Query = () => {
   const streamQuery = useLoaderData() as APIDataType[]
+  const resultLength = streamQuery.length
+
+  const { name } = useParams()
+  const formattedName = name ? formatName(name) : ""
 
   return (
     <>
-      <DisplaySelectionSection title="Recommended for you" data={streamQuery} />
+      {console.info(name)}
+      <DisplaySelectionSection
+        title={`Found ${resultLength} result${resultLength > 1 ? "s" : ""} for ${formattedName}`}
+        data={streamQuery}
+      />
     </>
   )
 }
